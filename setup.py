c409861a4e7574e5cf2fec0a88a684d93a1b2c60
Vikram Sandhu <vikram.sandhu@outlook.com>
2020-08-20 13:51:17 -0700

    Adding more to setup file

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="AGUC8",
+    version="0.1",
+    description="ARTIQ support for Newport AGUC8 piez motor controller",
+    author="OregonIons",
+    url="https://github.com/ARTIQ-Controllers/AGUC8",
+    download_url="https://github.com/ARTIQ-Controllers/AGUC8",
     install_requires=["sipyco", "pyserial"],
     packages=find_packages(),
     entry_points={
